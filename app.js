335fa6b2bbf5663a7d4759cc00dcab5821c9d361
vic-lee <96267768+vic-lee@users.noreply.github.com>
2021-12-30 21:50:11 -1000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+/*
+Arrays are a list of items and can hold many values.
+Values can be accessed by their index number, which starts at zero (zero based index)
+Arrays have build-in methods and properties, which you'll have some practice with through this assignment.
+Please console.log each question to check your work.
+*/
+
+/* sort() sorts elements of an array
+1. Ronald McDonald wants to have all of his employees listed in alphabetical order. Please help this clown. 
+*/
+const characters = ['Grimmace', 'Birdie', 'Mayor McCheese', 'Hamburglar', 'Officer Big Mac', 'The Professor', 'McNuggest Buddies'];
+
+
+/*unshift() adds one or more elements to the beginning of an array
+2. Big time Hollywood star Jason Bateman wants to be the first in line to get the new iPhone. Make this man's dreams come true.
+*/
+const iphoneQueue = ['Will Arnett', 'Sean Hayes', 'Justine Bateman', 'Amanda Anka'];
+
+
+/*shift() removes the first element from an array
+3. Update the play list by removing the first song title. Now, that's what I call music.
+*/
+const playList = ['All I Want for Christmas', 'Macarena', 'Mambo No. 5', 'All The Stars', 'Sunflower'];
+
+
+/*splice() removes or replaces existing elements and/or adds new elements
+4. The White House called and wants you to update the President's list below. Remove the names that don't belong and replace with the correct Presidents. 
+The list below is in chronological order. The country is counting on you.
+*/
+const presidentsClub = ['John Quincy Adams', 'Andrew Jackson', 'Mario Van Peebles', 'Christopher Wallace', 'John Tyler'];
+
+
+/*push() adds one or more elements at the end of an array
+5. The Heisman Trophy is awarded to the nation's most outstanding collegiate football player. Keep the tradition going and add the 2021 recipient to the list below. 
+*/
+const heisman = ['Mariota', 'Henry', 'Jackson', 'Mayfield', 'Murray', 'Burrow', 'Smith'];
+
+
+/*pop() removes the last element from an array
+6. Kellogg's Pop-Tarts have been fueling our nations youth for years. While most of the flavors have been fan favorites, some unfortunately didn't make the cut.
+Remove the last failed flavor from the list.
+*/
+const flavors = ['blueberry', 'chocolate fudge', 'strawberry', 'fruit loops', 'red velvet', 'eggo frosted maple', 'red radish'];
 
